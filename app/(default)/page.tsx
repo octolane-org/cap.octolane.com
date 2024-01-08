57cf8eb5425254adcf3d3bdee71e2466ca4759f6
One Chowdhury <coffeeandone@gmail.com>
2024-01-07 19:03:21 -0800

    SEO fix

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Simple",
-  description: "Page description",
+  title: "ByeByeCarta.com",
+  description: "Open Source Cap Table Manager",
 };
 
 import Hero from "@/components/hero-home";
