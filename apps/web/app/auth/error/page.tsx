3f3414c456652c3e7b0695f0617e4abbc9eb9524
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2024-01-11 00:08:05 +0600

    Nextauth options

---
+++ apps/web/app/auth/error/page.tsx
@@ -0,0 +1,33 @@
+import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/options";
+import Container from "@/components/Container";
+import { GoogleLoginButton } from "@/components/GoogleLoginButton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import type { ServerPageProps } from "@/types/common.type";
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+const AuthErrorPage = async ({
+  searchParams,
+}: ServerPageProps<any, { error: string }>) => {
+  const session = await getServerSession(nextAuthOptions);
+
+  if (session?.user) {
+    redirect("/");
+  }
+
+  return (
+    <Container>
+      <div className="py-10 flex flex-col items-center justify-center gap-8">
+        <Alert variant="destructive">
+          <ExclamationCircleIcon className="h-4 w-4" />
+          <AlertTitle>Error while sign in!</AlertTitle>
+          <AlertDescription>{searchParams.error}</AlertDescription>
+        </Alert>
+        <GoogleLoginButton />
+      </div>
+    </Container>
+  );
+};
+
+export default AuthErrorPage;
---
+++ apps/web/core/constants/configs.ts
@@ -0,0 +1,38 @@
+const isProduction = process.env.NODE_ENV === "production";
+
+export const configuration = {
+  site: {
+    name: "Captable",
+    description: "Open source cap table management software",
+    image: "/assets/hero.png",
+    themeColor: "#ffffff",
+    themeColorDark: "#0a0a0a",
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+    siteName: "Captable",
+    twitterHandle: "octolane_app",
+    githubHandle: "octolane-org",
+  },
+  isProduction,
+  environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
+  octolaneAPIKey: process.env.OCTOLANE_API_KEY,
+  sentry: {
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  },
+  posthog: {
+    apiKey: process.env.NEXT_PUBLIC_POSTHOG_API_KEY,
+    apiHost: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+  },
+  auth: {
+    secret: process.env.NEXTAUTH_SECRET as string,
+    google: {
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  metaphoreApiKey: process.env.METAPHOR_API_KEY,
+  openaiApiKey: process.env.OPENAI_API_KEY,
+  youApiKey: process.env.YOU_API as string,
+};
+
+export const FINGERPRINT_HEADER = "x-fingerprint";
+export const X_CSRF_TOKEN_HEADER = "X-CSRF-Token";
