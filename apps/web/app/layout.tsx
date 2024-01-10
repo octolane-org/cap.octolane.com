d8ee731595ce3bba2a82f5b546b3d64ecad20b4d
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2024-01-10 14:21:08 +0600

    Header component for homepage

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,5 @@
 import { Inter } from "next/font/google";
 
-import Header from "@/components/ui/header";
-
 import "./css/style.css";
 
 const inter = Inter({
@@ -26,7 +24,6 @@ export default function RootLayout({
         className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}
       >
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
-          <Header />
           {children}
         </div>
       </body>
