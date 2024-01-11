d82698a4b625209eec7b520d14abaeb50b341ee1
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2024-01-11 13:01:50 +0600

    Lint: Equity plans table done

---
+++ apps/web/app/equity-plans/new/page.tsx
@@ -0,0 +1,20 @@
+import Container from "@/components/ui/Container";
+import Tile from "@/components/ui/Tile";
+import { Button } from "@/components/ui/button";
+
+const NewEquityPlanPage = async () => {
+  return (
+    <div className="h-full min-h-svh my-8 flex flex-col items-center gap-8">
+      <Container>
+        <Tile>
+          <div className="flex items-center justify-between w-full">
+            <h1 className="text-2xl font-bold">Equity Plans</h1>
+            <Button>Add Equity Plan</Button>
+          </div>
+        </Tile>
+      </Container>
+    </div>
+  );
+};
+
+export default NewEquityPlanPage;
