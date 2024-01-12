928885cac8361c4818a64831e01d3f0399af9015
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2024-01-12 08:11:59 +0600

    Work in progress component

--- apps/web/components/WorkInProgress.tsx
+++ apps/web/components/WorkInProgress.tsx
@@ -1,8 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faBarsProgress } from "@fortawesome/free-solid-svg-icons";
+
+import Container from "./ui/Container";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+
 const WorkInProgress = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <h1 className="text-3xl font-bold">Work in progress...</h1>
-    </div>
+    <Container className="flex flex-col items-center justify-center h-[70vh] max-w-2xl">
+      <Alert className="border-amber-400 bg-amber-50">
+        <FontAwesomeIcon
+          className="h-4 w-4 !text-amber-600"
+          icon={faBarsProgress}
+        />
+        <AlertTitle className="text-amber-600">Work in progress!</AlertTitle>
+        <AlertDescription className="text-amber-600">
+          This page is still under construction. Please check back later.
+        </AlertDescription>
+      </Alert>
+    </Container>
   );
 };
 
